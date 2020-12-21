f76c44af8f7cc565896894d48750e7d1c24b4b84
hunglv16 <v.hunglv16@vinid.net>
2020-12-21 11:02:32 +0700

    Initial Commit

---
+++ RaceTest/RaceTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaceTestApp.swift
+//  RaceTest
+//
+//  Created by Zeno on 21/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct RaceTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
